ff90481ecf41c43740a82ef96d1e6b129c5887b8
Simone Deidier <simone.deidier@mail.polimi.it>
2023-07-01 19:09:54 +0200

    There was a q

--- database/database.sql
+++ database/database.sql
@@ -1,4 +1,4 @@
-qCREATE DATABASE  IF NOT EXISTS `tiw_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `tiw_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `tiw_project`;
 -- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
 --
